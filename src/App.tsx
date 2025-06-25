b61e16c95d8a3c00561e99308cf4dd4e30ca6397
jbaizack <jbaizack222@gmail.com>
2025-06-25 15:38:46 -0500

    Created card component and used tailwindcss for styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import Card from './components/Card'; 
+
+
+const cardInfo = {
+  coverImageLink: '/blog-card/living-room.jpg', 
+  category: 'Interior', 
+  title: 'Top 5 Living Room Inspirations', 
+  description: 'Curated vibrants colors for your living, make it pop & calm in the same time.', 
+  CTALink: ''
+}
 
 export default function App() {
   return (
-    <></>
+    <div className="wrapper">
+      <Card info={cardInfo}/>
+    </div>
   )
 }
